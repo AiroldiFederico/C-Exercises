f0d39de31341f4a375555b9d297d88f5e4650689
Federico Airoldi <airoldibusiness@gmail.com>
2025-12-29 18:54:29 +0100

    es1 3

--- Es_lab_7/esercizio_1.c
+++ Es_lab_7/esercizio_1.c
@@ -1,86 +1,65 @@
-/*
-Es0a: Scrivere un programma che:
-- Legge da tastiera una matrice 4X4 di interi
-- La stampa a video
-- Conta quanti numeri dispari ci sono in tutta la matrice
-- Conta quanti numeri primi ci sono in ogni riga della matrice
-
-NB: compilazione con gcc Es0a_contaDispariEPrimi.c -lm
-*/
-
-
-
 #include <stdio.h>
-
 
 int main(void){
 
-	int mtx [4][4];
-	int i,j,ref,even,odd,prime,isPrime;
-	i=j=even=odd=prime=0;
+    int mtx[4][4];
+    int even = 0, odd = 0; 
+    
+    printf("Enter values for a 4x4 matrix:\n");
+
+    // Input
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            scanf("%d", &mtx[i][j]);
+        }
+    }
+
+    printf("\nMatrix: \n");
 
 
-	printf("enter values for a 4x4 matrix\n");
+    for (int i = 0; i < 4; ++i) // Ciclo Righe
+    {
+        int primesInRow = 0; // Resetta il contatore per OGNI riga
 
-	for (int i = 0; i < 4; ++i)
-	{
-		for (int j = 0; j < 4; ++j)
-		{
-			scanf("%d", &mtx[i][j]);
-		}
-	}
+        for (int j = 0; j < 4; ++j) // Ciclo Colonne
+        {
+            int currentNum = mtx[i][j];
 
-	printf("\nMatrix: \n");
+            // 1. Controllo Pari/Dispari
+            if(currentNum % 2 == 0){
+                even++;
+            } else {
+                odd++;
+            }
 
-	for (int a = 0; a < 4; ++a)
-	{
-		for (int b = 0; b < 4; ++b)
-		{
+            // Stampa numero
+            printf("%d ", currentNum);
 
-			if(mtx[a][b]%2 == 0){
-				even++;
-			} else {
-				odd++;
-			}
+            // 2. Controllo NUMERO PRIMO 
+            int isPrime = 1;
+            
+            if (currentNum <= 1) {
+                isPrime = 0; 
+            } else {
+                // Controlliamo divisibilità da 2 fino a metà del numero
+                for (int k = 2; k <= currentNum / 2; k++) {
+                    if (currentNum % k == 0) {
+                        isPrime = 0; 
+                        break; 
+                    }
+                }
+            }
 
-			printf("%d ", mtx[a][b]);
+            if (isPrime == 1){
+                primesInRow++;
+            }
+        }
+        printf(" -> Primes in this row: %d\n", primesInRow);
+    }
 
+    printf("\nTotal Statistics:\n");
+    printf("Even numbers: %d\n", even);
+    printf("Odd numbers: %d\n", odd);
 
-			isPrime = 1;
-
-			for (int c = 0; c < 4; ++c)
-			{
-				
-				for (int d = 0; d < 4; ++d)
-				{
-					int ma1 = mtx[a][b];
-					int ma2 = mtx[c][d];
-
-					if(ma1 != 1 && ma1 != ma2 && ma2 != 1){
-						int conf = ma1%ma2;
-						if(conf==0){
-							isPrime = 0;
-						}
-					}
-
-					if(ma1 == 1){
-						isPrime = 0;
-					}
-				}
-			}
-
-			if (isPrime == 1){
-				prime++;
-				isPrime = 1;
-			}
-
-
-		}
-		printf("\n");
-	}
-
-
-	printf("\nIn the matrix there are %d even numbers\n%d odd numbers\nand there are a %d prime numbers", even,odd,prime);
-
-	return 0;
+    return 0;
 }
